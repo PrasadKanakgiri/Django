c986c079f39d8c35b62febcac40b2071063663d3
Prasad Kanakgiri <kanakgiriprasad26@gmail.com>
2024-07-02 23:09:59 +0530

    adding tailwind in Django and super user

--- Django/Django/settings.py
+++ Django/Django/settings.py
@@ -38,7 +38,13 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "demo",
+    "tailwind",
+    "theme",
+    "django_browser_reload",
 ]
+
+TAILWIND_APP_NAME = 'theme'
+INTERNAL_IPS = ['127.0.0.1']
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
@@ -48,6 +54,8 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+
+    "django_browser_reload.middleware.BrowserReloadMiddleware",
 ]
 
 ROOT_URLCONF = "Django.urls"
@@ -123,3 +131,5 @@ STATICFILES_DIRS= [os.path.join(BASE_DIR, 'static')]
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+NPM_BIN_PATH = r"C:\Users\Admin\AppData\Roaming\npm\npm.cmd "
