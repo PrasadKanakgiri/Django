04a39441456e22114dce152ed8c51dbf776595a6
Prasad Kanakgiri <kanakgiriprasad26@gmail.com>
2024-07-02 12:31:32 +0530

    templates

--- Django/Django/views.py
+++ Django/Django/views.py
@@ -1,7 +1,9 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def home(request):
-    return HttpResponse("Hello!!! You are at Django Home page")
+    # return HttpResponse("Hello!!! You are at Django Home page")
+    return render(request, 'index.html')
 
 def about(request):
     return HttpResponse("Hello!!! You are at Django About page")
