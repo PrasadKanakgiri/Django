4fbe90712628519ddb83ea417f4a19fbd2a017e7
Prasad Kanakgiri <kanakgiriprasad26@gmail.com>
2024-07-03 15:24:51 +0530

    Handling  model and urls

--- Django/demo/urls.py
+++ Django/demo/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.demo, name="demo"),
+    path('<int:chai_id>/', views.chai_detail, name='chai_detail'),
 ]
--- Django/demo/views.py
+++ Django/demo/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
-
+from .models import ChaiVariety
+from django.shortcuts import render, get_object_or_404
 # Create your views here.
 
 def demo(request):
-    return render(request, 'demo/demo.html')
+    chais = ChaiVariety.objects.all()
+    return render(request, 'demo/demo.html', {'chais': chais})
+
+def chai_detail(request, chai_id):
+  chai = get_object_or_404(ChaiVariety, pk=chai_id)
+  return render(request, 'demo/chai_detail.html', {'chai': chai})
